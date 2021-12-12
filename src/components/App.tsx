2d6b9c93cd3be33065190cb1b8d9435b513f1baa
pure-adachi <takuya_adachi@pure-system.com>
2021-12-13 00:44:13 +0900

    add PUBLIC_URL

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
+import NotMatch from "./pages/NotMatch";
 import { client } from "../middleware";
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL ? "friend-ship-client" : ""}>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<NotMatch />} />
         </Routes>
       </Router>
     </ApolloProvider>
---
+++ src/components/pages/NotMatch/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const NotMatch = () => {
+  return <div>Not Match</div>;
+};
+
+export default NotMatch;
